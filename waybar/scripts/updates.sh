4b40de9ca611ab29fc76aef7086ff1a791ea9f15
SourceTheProto <abicknell05@proton.me>
2024-11-23 05:40:03 -0700

    Swapped KDE polkit with hyprpolkit and yay with paru

--- waybar/scripts/updates.sh
+++ waybar/scripts/updates.sh
@@ -7,7 +7,7 @@ if ! updates_arch=$(checkupdates 2> /dev/null | wc -l); then
 	updates_arch=0
 fi
 
-if ! updates_aur=$(yay -Qua --aur --quiet | wc -l); then
+if ! updates_aur=$(paru -Qua --aur --quiet | wc -l); then
 	updates_aur=0
 fi
 
